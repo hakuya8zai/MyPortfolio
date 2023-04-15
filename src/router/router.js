eb3800c1d64582652c4df0b2b28545336b181bda
HuiKaiChung <hakuya8zai@gmail.com>
2023-04-15 16:02:41 +0800

    router test

--- src/router/router.js
+++ src/router/router.js
@@ -3,16 +3,11 @@ import Home from "../views/Home.vue";
 const routes = [
   {
     path: "/MyPortfolio",
-    redirect: "/Home",
+    name: "Home",
     component: Home, //這是靜態載入，一開始就抓，點了以後不會再用網路，建議大的用這個
   },
   {
-    path: "/Home",
-    name: "Home",
-    component: () => import("../views/Home.vue"), //這是靜態載入，一開始就抓，點了以後不會再用網路，建議大的用這個
-  },
-  {
-    path: "/About",
+    path: "/MyPortfolio/About",
     name: "About",
     component: () => import("../views/About.vue"), //這是動態載入，點了才抓，建議小的用這個
   },
