81b10f5f568fa1d2579f7f772594617b58b1d270
HuiKaiChung <hakuya8zai@gmail.com>
2023-06-26 16:45:39 +0800

    Update Resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -7,7 +7,7 @@
                 </div>
                 <div class="resume-card row row-cols-1 row-cols-lg-2 row-cols-lg-3 justify-content-center">
                     <div class="col pdm-card">
-                        <a href="https://firebasestorage.googleapis.com/v0/b/frankportfolio-e3ae3.appspot.com/o/Resume%2F20230623_PDM_ChineseResume.pdf?alt=media&token=2daa7491-53e8-42a9-bc09-bcb1bb685b4a" target="_blank" class="nlink-style" @mouseover="PmHover=true" @mouseout="PmHover=false">
+                        <a href="https://firebasestorage.googleapis.com/v0/b/frankportfolio-e3ae3.appspot.com/o/Resume%2F20230626_PDM_ChinesResume.pdf.pdf?alt=media&token=fac55a29-7330-4c51-a21b-904f5d308764" target="_blank" class="nlink-style" @mouseover="PmHover=true" @mouseout="PmHover=false">
                             <div class="card mt-3 mb-5 bg-dark text-bg-dark">
                                 <img v-show="!PmHover" src="../assets/Resume/PM_Before.png" class="card-img-top img-fluid px-5 pt-4" alt="...">
                                 <img v-show="PmHover"  src="../assets/Resume/PM_After.png" class="card-img-top img-fluid px-5 pt-4" alt="...">
@@ -19,19 +19,19 @@
                         </a>
                     </div>
                     <div class="col fe-card">
-                        <a href="https://firebasestorage.googleapis.com/v0/b/frankportfolio-e3ae3.appspot.com/o/Resume%2F20230623_FED_ChineseResume.pdf?alt=media&token=e0dbc126-c6bb-44a9-a19f-27de6c84c113" target="_blank" class="nlink-style" @mouseover="FeHover=true" @mouseleave="FeHover=false">
+                        <a href="https://firebasestorage.googleapis.com/v0/b/frankportfolio-e3ae3.appspot.com/o/Resume%2F20230626_FED_ChinesResume.pdf?alt=media&token=b691d045-b259-486e-9571-2be3f8865ba3" target="_blank" class="nlink-style" @mouseover="FeHover=true" @mouseleave="FeHover=false">
                             <div class="card mt-3 mb-5 bg-dark text-bg-dark">
                                 <img v-show="!FeHover" src="../assets/Resume/FE_Before.png" class="card-img-top img-fluid px-5 pt-4" alt="...">
                                 <img v-show="FeHover"  src="../assets/Resume/FE_After.png" class="card-img-top img-fluid px-5 pt-4" alt="...">
                                 <div class="card-body">
-                                    <h5 class="card-title text-warning">Frontend Engineer</h5>
+                                    <h5 class="card-title text-warning">Frontend Developer</h5>
                                     <p class="card-text">Transforming the ideas of designers into reality through continuous learning and a pursuit of excellence, ultimately providing users with high-quality web experiences.</p>
                                 </div>
                             </div>
                         </a>
                     </div>
                     <div class="col ux-card">
-                        <a href="https://firebasestorage.googleapis.com/v0/b/frankportfolio-e3ae3.appspot.com/o/Resume%2F20230623_UXD_ChineseResume.pdf?alt=media&token=686f3b82-8bd1-4869-a094-ea22ccdf948f" target="_blank" class="nlink-style" @mouseover="UxHover=true" @mouseleave="UxHover=false">
+                        <a href="https://firebasestorage.googleapis.com/v0/b/frankportfolio-e3ae3.appspot.com/o/Resume%2F20230626_UXD_ChinesResume.pdf.pdf?alt=media&token=fe98f9dd-28a5-4edd-8316-664c8aa7c2b0" target="_blank" class="nlink-style" @mouseover="UxHover=true" @mouseleave="UxHover=false">
                             <div class="card mt-3 mb-5 bg-dark text-bg-dark">
                                 <img v-show="!UxHover" src="../assets/Resume/UX_Before.png" class="card-img-top img-fluid px-5 pt-4" alt="...">
                                 <img v-show="UxHover"  src="../assets/Resume/UX_After.png" class="card-img-top img-fluid px-5 pt-4" alt="...">
